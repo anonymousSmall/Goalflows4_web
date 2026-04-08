7ee480fb02de8bd349f20647fc9dbd0402e76556
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-08 10:46:14 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -71,7 +71,7 @@ function Header() {
                         พร้อมบริการซ่อม สอบเทียบ อบรมการใช้งาน<br/>
                     </p>
                     <button className="mt-6 px-6 py-3 bg-[#003b6e] text-white rounded-lg hover:bg-[#002a4d] transition">
-                        ติดต่อเรา
+                        ติดต่อโกลโฟลฯ
                     </button>
                 </div>
 
