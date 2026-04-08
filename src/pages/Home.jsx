33208bbce4df5646ac2de0312e2ca1577b5dea8d
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-08 11:44:38 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Productforsale from '../components/Productforsale'
 import Community from '../components/Community'
 import Pixelgrade from '../components/Pixelgrade'
 import Clients from '../components/Clients'
+import OurCustomer from '../components/OurCustomer'
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <Header />
       <Productforsale />
       <Community />
+      <OurCustomer/>
       {/* <Pixelgrade /> */}
       {/* <Clients /> */}
     </>
