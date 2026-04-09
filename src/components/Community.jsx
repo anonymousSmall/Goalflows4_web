95f4be749b6e31e6ae3f6a67e5f21faeacf39350
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-09 11:16:44 +0700

    Update Community.jsx

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -4,19 +4,50 @@ import bgImage from "../assets/image/new/03.1BG.png";
 
 function Community() {
     return (
-        <section className="relative w-full bg-white py-14 md:py-20">
-            <div className="relative max-w-7xl mx-auto overflow-hidden rounded-2xl shadow-xl group">
+        <div
+            className="relative w-full min-h-[500px] md:min-h-[600px] lg:min-h-[700px] flex items-center bg-cover bg-center"
+            style={{ backgroundImage: `url(${bgImage1})` }}
+        >
+            {/* overlay */}
+           {/* <div className="absolute inset-0 bg-black/35 opacity-50"></div> */}
 
-                {/* Background */}
-                <div className="h-[320px] sm:h-[380px] md:h-[460px]">
-                    <img
-                        src={bgImage}
-                        alt="BGGF"
-                        className="w-full h-full object-cover scale-105 group-hover:scale-110 transition duration-700"
-                    />
+            <div className="relative container mx-auto max-w-[1320px] px-6 py-12 flex flex-col-reverse md:flex-row items-center justify-between gap-10">
+
+                {/* LEFT */}
+                <div className="flex flex-col items-center md:items-start text-center md:text-left">
+
+                    {/* LOGO */}
+                    {/* <img
+                        src={branner1}
+                        alt="GOAL FLOW INSTRUMENT"
+                        className="w-[220px] sm:w-[300px] md:w-[420px] lg:w-[500px] object-contain"
+                    /> */}
+                     <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-[#0d3f66] mb-10 text-center md:text-left leading-tight tracking-wide">
+                        สินค้าที่จำหน่าย
+                    </h1>
+
+                    {/* DESCRIPTION */}
+                    <p className="mt-6 text-sm sm:text-base md:text-lg lg:text-xl font-medium text-white leading-relaxed max-w-[600px]">
+                        ● ซ่อม / สอบเทียบ <br/>
+                        ● อบรมการใช้งาน <br/>
+                        ● บำรุงรักษาเครื่องมือ เครื่องจักร<br/>
+                        ● ด้วยทีมงานผู้เชี่ยวชาญด้านเครื่องมือเฉพาะทางสำหรับอุตสาหกรรม<br/>
+                    </p>
+                    <button className="mt-6 px-6 py-3 bg-[#003b6e] text-white rounded-lg hover:bg-[#002a4d] transition">
+                        ติดต่อโกลโฟลฯ
+                    </button>
                 </div>
+
+                {/* RIGHT (SLIDER) */}
+                <div className="w-full md:w-[50%] flex justify-center">
+                    <div className="w-[80%] sm:w-[70%] md:w-full">
+                        <BrandSlider />
+                    </div>
+                </div>
+
             </div>
-        </section>
+
+        </div>
     )
 }
 
