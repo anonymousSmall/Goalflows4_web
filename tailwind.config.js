0c9084256f7560b485ce5c843ad8cc2f2a474615
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-10 14:43:00 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import { transform } from 'lodash'
+import { plugin } from 'postcss'
+import { fadeIn } from 'src'
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
