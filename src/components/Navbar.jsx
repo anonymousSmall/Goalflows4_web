e0ce49230529375d8b6b95d12265808f81d83c30
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-19 15:33:07 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,9 +41,13 @@ export default function Navbar() {
             </div>
 
             <div className="absolute left-0 top-full w-[260px] bg-white text-black shadow-xl opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300">
-              <div className="p-3 hover:bg-gray-100">เครื่องวัดขนาด</div>
-              <div className="p-3 hover:bg-gray-100">เครื่องมือวัด</div>
-              <div className="p-3 hover:bg-gray-100">เครื่องทดสอบ</div>
+              <div className="p-3 hover:bg-gray-100">เครื่องวัดขนาดชิ้นงานผลิต</div>
+              <div className="p-3 hover:bg-gray-100">เครื่องวัดชิ้นงานแบบสัมผัส</div>
+              <div className="p-3 hover:bg-gray-100">เครื่องเทสชิ้นงาน</div>
+              <div className="p-3 hover:bg-gray-100">เครื่องมือวัดชิ้นงาน</div>
+              <div className="p-3 hover:bg-gray-100">เครื่องมือตรวจสอบต่างๆ</div>
+              <div className="p-3 hover:bg-gray-100">เครื่องมือทดสอบห้องปฏิบัติการ</div>
+              <div className="p-3 hover:bg-gray-100">เครื่องชั่งน้ำหนัก</div>
             </div>
           </li>
 
@@ -104,8 +108,8 @@ export default function Navbar() {
               >
                 สินค้าของเรา
                 <span className="ml-2 text-lg font-semibold transition-transform duration-300">
-  {openMenu === "product" ? "−" : "+"}
-</span>
+                      {openMenu === "product" ? "−" : "+"}
+                </span>
               </div>
 
               {/* LEVEL 2 */}
@@ -116,12 +120,12 @@ export default function Navbar() {
                   className="p-4 pl-6 py-3 flex justify-between cursor-pointer border-b"
                   onClick={() => handleSubToggle("measure")}
                 >
-                  เครื่องวัดขนาด
+                  เครื่องวัดขนาดชิ้นงานผลิต
                   <span>{subMenu === "measure" ? "−" : "+"}</span>
                 </div>
 
                 <div className={`${subMenu === "measure" ? "block" : "hidden"} pl-10 bg-gray-100`}>
-                  <p className="py-2">2.5D</p>
+                  <p className="py-2">กล้องวัดขนาดชิ้นงาน2.5D</p>
                   <p className="py-2">Profile Projector</p>
                   <p className="py-2">CCD Camera</p>
                 </div>
@@ -131,7 +135,7 @@ export default function Navbar() {
                   className="p-4 pl-6 py-3 flex justify-between cursor-pointer border-b"
                   onClick={() => handleSubToggle("tool")}
                 >
-                  เครื่องมือวัด
+                  เครื่องวัดชิ้นงานแบบสัมผัส
                   <span
                     className={`ml-2 text-lg transition-transform duration-300 ${
                     openMenu === "tool" ? "rotate-45" : ""
@@ -142,9 +146,11 @@ export default function Navbar() {
                 </div>
 
                 <div className={`${subMenu === "tool" ? "block" : "hidden"} pl-10 bg-gray-100`}>
-                  <p className="py-2">Vernier</p>
-                  <p className="py-2">Micrometer</p>
-                  <p className="py-2">Height Gauge</p>
+                  <p className="py-2">CONTOUR</p>
+                  <p className="py-2">ROUNGNESS</p>
+                  <p className="py-2">SURFTEST</p>
+                  <p className="py-2">COORDINATE MEASURING MACHINE</p>
+                  <p className="py-2">PROTABLE CMM ARM</p>
                 </div>
 
                 {/* SUB 3 */}
@@ -152,14 +158,91 @@ export default function Navbar() {
                   className="p-4 pl-6 py-3 flex justify-between cursor-pointer"
                   onClick={() => handleSubToggle("test")}
                 >
-                  เครื่องทดสอบ
+                  เครื่องเทสชิ้นงาน
                   <span>{subMenu === "test" ? "−" : "+"}</span>
                 </div>
 
                 <div className={`${subMenu === "test" ? "block" : "hidden"} pl-10 bg-gray-100`}>
-                  <p className="py-2">Hardness</p>
-                  <p className="py-2">Tensile</p>
-                  <p className="py-2">Salt Spray</p>
+                  <p className="py-2">HARDNESS TEST</p>
+                  <p className="py-2">PUSH PULL TESTER</p>
+                  <p className="py-2">CUTTING / POLISHING MACHINE</p>
+                  <p className="py-2">TENSILE MACHINE</p>
+                  <p className="py-2">SALT SPRAY</p>
+                  <p className="py-2">OVEN</p>
+                </div>
+                
+                {/* SUB 4 */}
+                <div
+                  className="p-4 pl-6 py-3 flex justify-between cursor-pointer"
+                  onClick={() => handleSubToggle("test1")}
+                >
+                  เครื่องมือวัดชิ้นงาน
+                  <span>{subMenu === "test1" ? "−" : "+"}</span>
+                </div>
+
+                <div className={`${subMenu === "test1" ? "block" : "hidden"} pl-10 bg-gray-100`}>
+                  <p className="py-2">VERNIER CALIPER</p>
+                  <p className="py-2">MICROMETER</p>
+                  <p className="py-2">DIAL INDICATOR / DIAL TEST</p>
+                  <p className="py-2">HEIGHT GAUGE</p>
+                  <p className="py-2">THICKNESS GAUGE</p>
+                  <p className="py-2">HOLE TEST / BORE GAUGE</p>
+                  <p className="py-2">DIAL TEST</p>
+                </div>
+                
+                {/* SUB 5 */}
+                <div
+                  className="p-4 pl-6 py-3 flex justify-between cursor-pointer"
+                  onClick={() => handleSubToggle("test2")}
+                >
+                  เครื่องมือตรวจสอบต่างๆ
+                  <span>{subMenu === "test2" ? "−" : "+"}</span>
+                </div>
+
+                <div className={`${subMenu === "test2" ? "block" : "hidden"} pl-10 bg-gray-100`}>
+                  <p className="py-2">FEELIER GAUGE</p>
+                  <p className="py-2">SCALE LUPE</p>
+                  <p className="py-2">PIN GAUGE / GAUGE BLOCK</p>
+                  <p className="py-2">PLUG GAUGE / RING GAUGE</p>
+                  <p className="py-2">TORQUE WRENCH</p>
+                  <p className="py-2">GRANIT / CAST IRON SURFACE</p>
+                </div>
+                
+                {/* SUB 6 */}
+                <div
+                  className="p-4 pl-6 py-3 flex justify-between cursor-pointer"
+                  onClick={() => handleSubToggle("test3")}
+                >
+                  เครื่องมือทดสอบห้องปฏิบัติการ
+                  <span>{subMenu === "test3" ? "−" : "+"}</span>
+                </div>
+
+                <div className={`${subMenu === "test3" ? "block" : "hidden"} pl-10 bg-gray-100`}>
+                  <p className="py-2">THERMOMETER</p>
+                  <p className="py-2">CONDUCTIVITY</p>
+                  <p className="py-2">BRIX METER</p>
+                  <p className="py-2">TRUE METER</p>
+                  <p className="py-2">LIGHT METER</p>
+                  <p className="py-2">VOICE METER / SOUND METER</p>
+                  <p className="py-2">GAUSS METER</p>
+                  <p className="py-2">GLOSS METER</p>
+                  <p className="py-2">MAGNETIC FIELD TESTER</p>
+                  <p className="py-2">DEMAGNETIZER</p>
+                </div>
+                
+                {/* SUB 7 */}
+                <div
+                  className="p-4 pl-6 py-3 flex justify-between cursor-pointer"
+                  onClick={() => handleSubToggle("test4")}
+                >
+                  เครื่องชั่งน้ำหนัก
+                  <span>{subMenu === "test4" ? "−" : "+"}</span>
+                </div>
+
+                <div className={`${subMenu === "test4" ? "block" : "hidden"} pl-10 bg-gray-100`}>
+                  <p className="py-2">INDUSTRIAL BALANCE</p>
+                  <p className="py-2">HOSPITAL BALANCE</p>
+                  <p className="py-2">ANIMAL SCALE</p>
                 </div>
 
               </div>
@@ -178,7 +261,8 @@ export default function Navbar() {
               <div className={`${openMenu === "service" ? "block" : "hidden"} pl-6 bg-gray-50`}>
                 <p className="py-2">สอบเทียบ ISO17025</p>
                 <p className="py-2">ซ่อมเครื่องมือ</p>
-                <p className="py-2">อบรม</p>
+                <p className="py-2">บำรุงรักษาเครื่องมือ เครื่องจักร</p>
+                <p className="py-2">อบรมการใช้งาน</p>
               </div>
             </li>
 
