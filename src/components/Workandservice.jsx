42f665122b833d4696fe1fb748fec0ec279dff5a
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-21 14:36:35 +0700

    Update Workandservice.jsx

---
+++ src/components/Workandservice.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LayoutWorkservice1 from '../layouts/LayoutWorkservice1'
+import LayoutWorkservice2 from '../layouts/LayoutWorkservice2'
+
+const Workandservice = () => {
+    return (
+        <>
+            <LayoutWorkservice1 />
+            <LayoutWorkservice2 />
+        </>
+    )
+}
+
+export default Workandservice
