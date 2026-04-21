f9cfc90316f928910a5879fffebf2665618bc569
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-21 14:34:13 +0700

    Add files via upload

---
+++ src/pages/Workandservices.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Workandservice from '../components/Workandservice'
+
+const Workandservices = () => {
+    return (
+        <>
+            <Workandservice />
+        </>
+    )
+}
+
+export default Workandservices
