7306fe93f9b77accab5445de10fe6497db472f8b
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-22 15:58:38 +0700

    Add files via upload

---
+++ src/pages/Goalflows.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Goalflows1 from '../components/Goalflows';
+
+const Goalflows = () => {
+  return (
+    <div>
+        <Goalflows1 />
+    </div>
+  )
+}
+
+export default Goalflows
