f116afb458f7fd8baaf5d2bd158954e6aaffd732
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-22 11:15:58 +0700

    Update Workandservice.jsx

--- src/components/Workandservice.jsx
+++ src/components/Workandservice.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import LayoutWorkservice1 from '../layouts/LayoutWorkservice1'
 import LayoutWorkservice2 from '../layouts/LayoutWorkservice2'
+import BackgroundStack from '../components/BackgroundStack'
 
 const Workandservice = () => {
     return (
-        <>
+        <div className="relative">
+
+            {/* 🔥 Background ต่อกัน */}
+            <BackgroundStack />
+
+            {/* Content */}
             <LayoutWorkservice1 />
             <LayoutWorkservice2 />
-        </>
+
+        </div>
     )
 }
 
