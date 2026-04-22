064f1f77435e72aa97af52cc544ea4f26949cecb
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-22 11:43:53 +0700

    Update LayoutWorkservice1.jsx

--- src/layouts/LayoutWorkservice1.jsx
+++ src/layouts/LayoutWorkservice1.jsx
@@ -5,9 +5,13 @@ import bgSection from "../assets/image/workservice/BG04.01BG.png"; // ✅ พื
 
 const LayoutWorkservice1 = () => {
   return (
-    <section
-
-            >
+<section
+  className="
+    relative
+    pt-24 md:pt-32 lg:pt-40   /* ✅ ดันลงจาก navbar */
+    pb-12 md:pb-16
+  "
+>
                 {/* Overlay */}
                 <div className="absolute inset-0"></div>
     
