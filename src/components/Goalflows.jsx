e8b508e5534d3baa0301f08be2aed65e44190060
anonymousSmall <52340774+anonymousSmall@users.noreply.github.com>
2026-04-23 12:59:57 +0700

    Update Goalflows.jsx

--- src/components/Goalflows.jsx
+++ src/components/Goalflows.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layoutgoalflows1 from '../layouts/Layoutgoalflows1'
 import Layoutgoalflows2 from '../layouts/Layoutgoalflows2'
+import Clients from '../components/Clients'
 
 const goalflows = () => {
   return (
